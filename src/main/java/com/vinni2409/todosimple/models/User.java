e20fbad3ca76e27a8f779a6d594e74e7db519562
vinni2409 <viniciusfurtado230@gmail.com>
2026-03-21 16:07:06 -0300

    Create Task Model

--- src/main/java/com/vinni2409/todosimple/models/User.java
+++ src/main/java/com/vinni2409/todosimple/models/User.java
@@ -1,5 +1,7 @@
 package com.vinni2409.todosimple.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,42 +12,41 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "User.TABLE_NAME")
+@Table(name = User.TABLE_NAME)
 public class User {
 
-    
-    public interface CreateUser{}
-    public interface UpdateUser{}
-
+    public interface CreateUser {}
+    public interface UpdateUser {}
 
     public static final String TABLE_NAME = "user";
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true) // Garantir que nunca irá duplicar o valor
+    @Column(name = "id", unique = true)
     private Long id;
- 
+
     @Column(name = "username", length = 100, nullable = false, unique = true)
     @NotNull(groups = CreateUser.class)
     @NotEmpty(groups = CreateUser.class)
-    @Size(min = 4, max= 100)
+    @Size(min = 4, max = 100)
     private String username;
 
     @JsonProperty(access = Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
     @NotNull(groups = {CreateUser.class, UpdateUser.class})
     @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
-    @Size(groups = { CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>(),
-
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
     public Long getId() {
         return this.id;
@@ -71,7 +72,13 @@ public class User {
         this.password = password;
     }
 
-    
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -82,11 +89,11 @@ public class User {
         if (!(obj instanceof User))
             return false;
         User other = (User) obj;
-        if (this.id == null)
+        if (this.id == null) {
             if (other.id != null)
                 return false;
-            else if (!this.id.equals(other.id))
-                return false;
+        } else if (!this.id.equals(other.id))
+            return false;
 
         return Objects.equals(this.id, other.id)
             && Objects.equals(this.username, other.username)
@@ -100,8 +107,4 @@ public class User {
         result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
         return result;
     }
-
-    
-    
 }
-
