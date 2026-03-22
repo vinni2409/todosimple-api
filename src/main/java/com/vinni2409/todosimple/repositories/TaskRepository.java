7af45b807c85cc0e5ce2946a22ddaf228dba7871
vinni2409 <viniciusfurtado230@gmail.com>
2026-03-22 15:36:11 -0300

    Create Task Repository

---
+++ src/main/java/com/vinni2409/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,26 @@
+package com.vinni2409.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.vinni2409.todosimple.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{
+    
+   List<Task> findByUser_Id(Long id);
+
+   /*@Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")// : -> se refere ao parametro, ou seja o id 
+   List<Task> findByUser_Id(@Param ("id")Long id); */
+   
+                // Os outros estão comentados por ter sido apanas para aprendizagem
+
+                
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+    // List<Task> findByUser_Id(@Param("id") Long id);
+
+}
